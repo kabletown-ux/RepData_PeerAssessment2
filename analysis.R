ad10fbfcc07572e46f1d4e767ba1df0e3311ad7e
rruiz201 <richard_ruiz@cable.comcast.com>
2015-09-22 15:36:53 -0400

    Added local data caching and loading optimizations

--- analysis.R
+++ analysis.R
@@ -1,3 +1,38 @@
 library( ggplot2 )
 
-rawData <- read.csv( "..\data\repdata-data-StormData.csv" )
+debug <- TRUE
+png <- TRUE
+large <- FALSE
+stormData <- NULL
+
+loadData <- function() {
+    
+    # Conditional data loading: Pull from file...
+    if ( is.null( stormData ) ) { 
+        
+        # Conditional dataset size: full, refreshed, data sets...
+        if ( large ) {
+            
+            if ( debug ) print( "Using BIG-ASSED versions of stormData..." )
+            stormData <<- read.csv( "../data/repdata-data-StormData.csv" )
+            
+        } else { # ...or smaller samples
+            
+            if ( debug ) print( "Using TOY versions of stormData..." )
+            stormData <<- read.csv( "../data/stormDataSample.csv" )
+        }
+        
+    } else { # or use cached copy
+        
+        if ( debug ) print( "Using cached versions of stormData..." )
+    }
+}
+sampleData <- function() {
+    
+    loadData()
+    set.seed( 2112 )
+    ## grab random rows
+    stormDataSample <<- stormData[ sample( nrow( stormData ), size = 10000, replace = F ), ]
+    
+    write.csv( stormDataSample, file = "../data/stormDataSample.csv" )
+}
