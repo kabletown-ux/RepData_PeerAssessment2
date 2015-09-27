f9335dccb8bc5604edd3307f4290d8828ea8fce2
rruiz201 <richard_ruiz@cable.comcast.com>
2015-09-27 18:13:13 -0400

    Prototyped analysis and graphing

--- analysis.R
+++ analysis.R
@@ -5,6 +5,8 @@ png <- TRUE
 large <- FALSE
 stormData <- NULL
 
+## proper data loading would point to URL, so that everybody's starting w/ the same data source 
+## This is *not* the same as the same data set
 loadData <- function() {
     
     # Conditional data loading: Pull from file...
@@ -29,10 +31,83 @@ loadData <- function() {
 }
 sampleData <- function() {
     
-    loadData()
+    if ( debug ) {
+        print( system.time( loadData() ) )
+    } else {
+        loadData()
+    }
     set.seed( 2112 )
     ## grab random rows
     stormDataSample <<- stormData[ sample( nrow( stormData ), size = 10000, replace = F ), ]
     
     write.csv( stormDataSample, file = "../data/stormDataSample.csv" )
 }
+cleanData <- function() {
+    
+    ## remove superfluous columns (this matters w/ large datasets!)
+    ## deal w/ borked strings
+    ## impute missing values
+}
+doFatalities <- function() {
+    
+    loadData()
+    fatalities <- aggregate( stormData[ "FATALITIES" ], by = stormData[ c( "EVTYPE" ) ], FUN = sum, na.rm = TRUE )
+    orderedFatalities <- fatalities[ order( -fatalities$FATALITIES ), ]
+    orderedFatalities <- orderedFatalities[ which( orderedFatalities$FATALITIES > 0 ), ]
+    print( orderedFatalities )
+    ggplot( data = orderedFatalities, aes( x = orderedFatalities$EVTYPE, y = orderedFatalities$FATALITIES, fill = EVTYPE ) ) + 
+        geom_bar( stat = "identity" ) +
+        theme( axis.text.x = element_text( angle = 45, hjust = 1 ) ) +
+        xlab( "Type of Event" ) +
+        ylab("Fatality Count" ) + 
+        ggtitle( "NOAA's Highest Fatality Counts" )
+}
+doInjuries <- function() {
+    
+    loadData()
+    injuries <- aggregate( stormData[ "INJURIES" ], by = stormData[ c( "EVTYPE" ) ], FUN = sum, na.rm = TRUE )
+    orderedInjuries <- injuries[ order( -injuries$INJURIES ), ]
+    orderedInjuries <- orderedInjuries[ which( orderedInjuries$INJURIES > 0 ), ]
+    print( orderedInjuries ) 
+
+    ggplot( orderedInjuries, aes( x = EVTYPE, y = INJURIES, fill = EVTYPE ) ) + 
+        geom_bar( stat="identity", position = "dodge" ) + 
+        labs( x = "Type of Event", y = "Fatality Count" ) + 
+        labs( title = "NOAA's Highest Injury Counts" ) +
+        theme( axis.text.x = element_text( angle = 45, vjust = 1, hjust=1 ) )
+}
+
+doCropDamages <- function() {
+    
+    loadData()
+    cropDamages <- aggregate( stormData[ "CROPDMG" ], by = stormData[ c( "EVTYPE" ) ], FUN = sum, na.rm = TRUE )
+    orderedCropDamages <- cropDamages[ order( -cropDamages$CROPDMG ), ]
+    # replace w/ top 10
+    #orderedCropDamages <- orderedCropDamages[ which( orderedCropDamages$CROPDMG > 0 ), ]
+    orderedCropDamages <- head( orderedCropDamages, 10 )
+    print( orderedCropDamages ) 
+    
+    ggplot( orderedCropDamages, aes( x = EVTYPE, y = CROPDMG, fill = EVTYPE ) ) + 
+        geom_bar( stat="identity", position = "dodge" ) + 
+        labs( x = "Type of Event", y = "Crop Damage (Thousands of Dollars?)" ) + 
+        labs( title = "NOAA's Largest Crop Damage" ) +
+        theme( axis.text.x = element_text( angle = 45, vjust = 1, hjust=1 ) )
+}
+
+doPropertyDamages <- function() {
+    
+    loadData()
+    propertyDamages <- aggregate( stormData[ "PROPDMG" ], by = stormData[ c( "EVTYPE" ) ], FUN = sum, na.rm = TRUE )
+    orderedPropertyDamages <- propertyDamages[ order( -propertyDamages$PROPDMG ), ]
+    # replace w/ top 10
+    #orderedPropertyDamages <- orderedPropertyDamages[ which( orderedPropertyDamages$PROPDMG > 0 ), ]
+    orderedPropertyDamages <- head( orderedPropertyDamages, 10 )
+    print( orderedPropertyDamages ) 
+    
+    ggplot( orderedPropertyDamages, aes( x = EVTYPE, y = PROPDMG, fill = EVTYPE ) ) + 
+        geom_bar( stat="identity", position = "dodge" ) + 
+        labs( x = "Type of Event", y = "Crop Damage (Thousands of Dollars?)" ) + 
+        labs( title = "NOAA's Largest Crop Damage" ) +
+        theme( axis.text.x = element_text( angle = 45, vjust = 1, hjust=1 ) )
+}
+
